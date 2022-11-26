37a2f64296f2a06c605ddd709dea7f6a0e513a03
Azad Hossen <azadhossen6677@gmail.com>
2022-11-26 22:19:48 +0600

    server setup ready

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+require('dotenv').config()
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+ console.log(process.env.DB_PASSWORD)
+
+//  Mongo DB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2kitjkk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('db connect');
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
